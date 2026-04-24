8c99b221a4e5d00bc940f692f349fc2469c4eb3d
gianni <94029291+giannidifra0@users.noreply.github.com>
2026-04-24 19:42:12 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import requests
+import time
+from telegram import Bot
+import os
+
+TOKEN = os.environ["8608528512:AAGUGc3Scl3_GYUgXke3803NonBX3S3XDhc"]
+CHAT_ID = os.environ["129897273"]
+
+def get_rate():
+    url = "https://api.exchangerate.host/latest?base=EUR&symbols=USD"
+    data = requests.get(url).json()
+    return data["rates"]["USD"]
+
+bot = Bot(token=TOKEN)
+
+while True:
+    rate = get_rate()
+    
+    if rate >= 1.20:
+        bot.send_message(chat_id=CHAT_ID, text=f"🚀 Cambio favorevole: {rate}")
+    
+    time.sleep(3600)
