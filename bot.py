3f077244ab0eb786df5b69746b1cdada1ac0f277
gianni <94029291+giannidifra0@users.noreply.github.com>
2026-04-24 19:58:59 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,14 +9,23 @@ CHAT_ID = 129897273
 def get_rate():
     url = "https://api.exchangerate.host/latest?base=EUR&symbols=USD"
     data = requests.get(url).json()
+
+    if "rates" not in data:
+        print("API error:", data)
+        return None
+
     return data["rates"]["USD"]
 
 bot = Bot(token=TOKEN)
 
 while True:
     rate = get_rate()
-    
+
+    if rate is None:
+        time.sleep(60)
+        continue
+
     if rate >= 1.20:
-        bot.send_message(chat_id=CHAT_ID, text=f"🚀 Cambio favorevole: {rate}")
-    
-    time.sleep(3600)
+        bot.send_message(chat_id=CHAT_ID, text=f"📈 Cambio favorevole: {rate}")
+
+    time.sleep(60)
